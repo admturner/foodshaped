7e77e9680344ceb0e03ade4263eda5f976f097d6
Adam Turner <support@adamturner.org>
2019-04-11 16:36:01 -0700

    Convert global data file from json to js with env processing
    Want to build the site with a different base url in development
    and in production, so switch to JavaScript for the site data file
    and process the Eleventy environment variable to conditionally
    set the base url.

---
+++ src/_data/site.js
@@ -0,0 +1,23 @@
+const env = process.env.ELEVENTY_ENV || 'production';
+const baseUrl = ( 'production' === env ) ? 'https://eternalpastry.com/' : 'http://localhost:8080/';
+
+module.exports = {
+  title: 'Eternal Pastry',
+  url: baseUrl,
+  author: {
+    name: 'Adam Turner'
+  },
+  tagline: [
+    'What even is pastry, really?',
+    'Regular human food.',
+    'Use mouth with food.',
+  ],
+  feed: {
+    subtitle: 'The Eternal Pastry food place',
+    filename: 'feed.atom',
+    path: '/feed.atom',
+    url: `${ baseUrl }feed.atom`,
+    id: baseUrl,
+  },
+  charset: 'utf-8',
+};
